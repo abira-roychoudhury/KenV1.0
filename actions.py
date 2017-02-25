caeacd47957b089efc2c5952d460f37c3c2ca8d0
Semicolon <Semicolon>
2017-02-25 19:01:06 +0530

    c2

--- actions.py
+++ actions.py
@@ -2,30 +2,61 @@ from random import randint
 import logging
 
 def LawFind(parameters):
+	logging.info("LawFind")
 
-	speech = ["H.R.861 To terminate the Enviornmental Protection Agency.","S.2266 H1B and L1 Visa Reform Act of 2015.","H.R.285 Healthcare Tax Relief and Mandate Repeal Act."]
+	if parameters["LawType"] : 
 
-	return {"response":speech[randint(0,2)], "contextOut":[{}]}
-
-
-def LawFindByKeyWord(parameters):
-	speech = {"Environment" : "H.R.861 To terminate the Environmental Protection Agency.",
+		speech = {"Environment" : "H.R.861 To terminate the Environmental Protection Agency.",
 			"Immigration" : "S.2266 H1B and L1 Visa Reform Act of 2015.",
 			"Health Care" : "H.R.285 Healthcare Tax Relief and Mandate Repeal Act."}
+		response = ""
 
-	response = ""
-
-	for law in parameters["LawType"]:
-		response = response + "This is what I found about "+str(law)+" : "+str(speech[law])
-
-	return {"response":response + "Do you want to hear more details?", "contextOut":[{"name":"userAnswer","lifespan":10, "parameters":{"lawid":123}}]}
+		for law in parameters["LawType"]:
+			response = response + " This is what I found about "+str(law)+" : "+str(speech[law])
+			LawId = 123
 	
+	else:
+
+		speech = ["H.R.861 To terminate the Enviornmental Protection Agency.","S.2266 H1B and L1 Visa Reform Act of 2015.","H.R.285 Healthcare Tax Relief and Mandate Repeal Act."]
+		response = speech[randint(0,2)]
+
+	return {"response":response+" Do you want more information about this law?", "contextOut":[{"name":"UserAnswer","lifespan":10, "parameters":{"LawId" : LawId }}]}
+
 def LawMoreInformation(parameters):
-	logging.info(parameters)
-	return {"response":"here is more information", "contextOut":[{}]}
+	logging.info("inside LawMoreInformation")
+
+	if parameters["MoreInfo"] == "yes":
+		#fetch more info
+		response = "fetch more info from DB."
+	else:
+		response = ""
+
+	#check DB for more laws 
+
+	if laws exists:
+		response = response + " Do you want to see some more laws." 
+		contextOut = [{"name":"MoreLaw", "lifespan" : 10, "parameters" : {"LawId" : parameters["LawId"]} }]
+	else:
+		response = "Thank you" #stop conversation
+		contextOut = []
+
+	return {"response":response, "contextOut":contextOut}
+
+
+def MoreLaw(parameters):
+	logging.info("inside NextLaw")
+
+	LawId = parameters["LawId"]
+
+	#fetch next law 
+
+	response = "Here is next Law."
+	
+	return {"response":response+" Do you want more information about this law?", "contextOut":[{"name":"UserAnswer","lifespan":10, "parameters":{"LawId":LawId}}]}
+
 	
 handler = {
 	"LawFind" : LawFind,
-	"LawFindByKeyWord" : LawFindByKeyWord,
-	"LawMoreInformation" : LawMoreInformation
+	"LawMoreInformation" : LawMoreInformation,
+	"MoreLaw" : MoreLaw
 }
